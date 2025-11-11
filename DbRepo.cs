0999cbd2264f1c2b4cf6443637a3f635b6efb7d9
BhavanaSrivatsavai03 <162876266+BhavanaSrivatsavai03@users.noreply.github.com>
2025-11-11 15:42:44 -0600

    Add files via upload

---
+++ DbRepo.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.Models.DBModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace LibraryManagementSystem.Models
+{
+    public class DbRepo:DbContext
+    {
+        public DbRepo(DbContextOptions<DbRepo> options):base(options)
+        {
+        }
+
+        public DbSet<Books> Books { get; set; }
+        public DbSet<Members> Members { get; set; }
+        public DbSet<Loans> Loans { get; set; }
+        public DbSet<Reservations> Reservations { get; set; }
+    }
+}
